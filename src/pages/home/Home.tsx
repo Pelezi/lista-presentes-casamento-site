407695a43d30823e07f0378e3fce728227fa6595
Alessandro <abcsandro@hotmail.com>
2025-04-03 01:18:29 -0300

    Refactor dialogue handling in Home component for improved clarity and functionality

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -125,39 +125,13 @@ const Home = () => {
         });
     };
 
-    const handleDismissClick = (dialogueNumber: number) => {
+    const handleDismissClick = () => {
         const dialogueElement = document.querySelector(`.${styles.dialogueBalloon}`);
         if (dialogueElement) {
             dialogueElement.classList.add(styles.fadeOut);
             setTimeout(() => {
-                switch (dialogueNumber) {
-                    case 1:
                         setShowDialogue1(false);
                         dialogueElement.classList.remove(styles.fadeOut);
-                        setShowDialogue2(true);
-                        break;
-                    case 2:
-                        setShowDialogue2(false);
-                        dialogueElement.classList.remove(styles.fadeOut);
-                        setShowDialogue3(true);
-                        break;
-                    case 3:
-                        setShowDialogue3(false);
-                        dialogueElement.classList.remove(styles.fadeOut);
-                        setShowDialogue4(true);
-                        break;
-                    case 4:
-                        setShowDialogue4(false);
-                        dialogueElement.classList.remove(styles.fadeOut);
-                        setShowDialogue5(true);
-                        break;
-                    case 5:
-                        setShowDialogue5(false);
-                        dialogueElement.classList.remove(styles.fadeOut);
-                        break;
-                    default:
-                        break;
-                }
             }, 500);
         }
     };
@@ -169,44 +143,8 @@ const Home = () => {
                     <p>Para escolher um presente, clique no botão 'Escolher Presente' abaixo do item desejado.
                     </p>
                     <p>
-                        Assim que você escolher, ele ficará indisponível para os outros convidados, evitando repetições. 😊</p>
-                    <button className={styles.dismissButton} onClick={() => handleDismissClick(1)}>Entendi</button>
-                </div>
-            )}
-            {showDialogue2 && (
-                <div className={styles.dialogueBalloon}>
-                    <p>Na aba {<MdOutlineShoppingCart />}, você pode ver os itens que escolheu.
-                    </p>
-                    <p>
-                        Se mudar de ideia, é possível remover um presente para liberar a escolha para outras convidadas.</p>
-                    <button className={styles.dismissButton} onClick={() => handleDismissClick(2)}>Entendi</button>
-                </div>
-            )}
-            {showDialogue3 && (
-                <div className={styles.dialogueBalloon}>
-                    <p>Este site não é uma loja online!
-                    </p>
-                    <p>
-                        É apenas uma lista de sugestões de presentes para evitar repetições e para que você possa ficar livre para escolher, sem estar presa a um presente específico.
-                    </p>
-                    <p>
-                        Depois de escolher seu presente, você pode comprá-lo na loja de sua preferência e trazê-lo para o chá de cozinha.
-                    </p>
-                    <p>
-                        O valor e local de compra ficam totalmente a seu critério!</p>
-                    <button className={styles.dismissButton} onClick={() => handleDismissClick(3)}>Entendi</button>
-                </div>
-            )}
-            {showDialogue4 && (
-                <div className={styles.dialogueBalloon}>
-                    <p>As imagens dos presentes são apenas ilustrativas. Não se preocupe em comprar exatamente o que aparece na foto! 😄</p>
-                    <button className={styles.dismissButton} onClick={() => handleDismissClick(4)}>Entendi</button>
-                </div>
-            )}
-            {showDialogue5 && (
-                <div className={styles.dialogueBalloon}>
-                    <p>Não se sinta obrigada a trazer um presente. Sua presença é mais importante pra mim do que qualquer presente! 💖</p>
-                    <button className={styles.dismissButton} onClick={() => handleDismissClick(5)}>Entendi</button>
+                        Você terá opção de enviar o dinheiro via pix, cartão ou boleto</p>
+                    <button className={styles.dismissButton} onClick={() => handleDismissClick()}>Entendi</button>
                 </div>
             )}
             <Title className={styles.title}>Bem-vindo a minha Lista de Presentes</Title>
